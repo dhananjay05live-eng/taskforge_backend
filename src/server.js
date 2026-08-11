8b99aedfb3b01a46a8363743c1772cc56e95c512
dhananjay05live-eng <dhananjay05live@gmail.com>
2026-08-11 23:25:33 +0530

    basic setup ready

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send({"message":"taskforce is alive"})
+})
+
+app.listen(process.env.PORT||8000,()=>{
+    console.log(`express app is listening `)
+})
